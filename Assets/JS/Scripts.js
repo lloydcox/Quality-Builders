0a17a7fc23ff1813285511c6214c0599add82740
Lloyd <lloydcox13@aol.com>
2020-07-30 15:37:20 +0100

    update
    updated partners, services and tesimonials, minor tweaks needed but other then that they are complete.

--- Assets/JS/Scripts.js
+++ Assets/JS/Scripts.js
@@ -1,37 +1,46 @@
+    // Carousel scriipts 
 
-       $('.your-class').slick({
-        dots: true,
-        infinite: false,
-        speed: 300,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
+
+$('.partners').slick({
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      infinite: true,
+    }
+},
+  {
+    breakpoint: 600,
+    settings: {
+    slidesToShow: 2,
+    slidesToScroll: 2
+    }
+},
+  {
+    breakpoint: 480,
+    settings: {
+    slidesToShow: 1,
+   slidesToScroll: 1
+  }
+}
           // You can unslick at a given breakpoint now by adding:
           // settings: "unslick"
           // instead of a settings object
-        ]
-      });
+ ]
+});
     
+$('.testimonials').slick({
+  // autoplay: true,
+  arrows: false,
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
