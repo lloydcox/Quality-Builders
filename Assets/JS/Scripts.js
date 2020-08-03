930750adc5d2e4858e8dbe49b3b631d1f8ffd451
Dermot Wilson <dermot.wilson@netmatters-scs.com>
2020-08-03 17:15:05 +0100

    First pull request merging Lloyd's repo into mine

---
+++ Assets/JS/Scripts.js
@@ -0,0 +1,57 @@
+    // Carousel scriipts 
+
+$(document).ready(function () {
+$('.main-carousel').slick({
+  autoplay: true,
+  arrows: true,
+  dots: false,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
+
+$('.partners').slick({
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      infinite: true,
+    }
+},
+  {
+    breakpoint: 600,
+    settings: {
+    slidesToShow: 2,
+    slidesToScroll: 2
+    }
+},
+  {
+    breakpoint: 480,
+    settings: {
+    slidesToShow: 1,
+   slidesToScroll: 1
+  }
+}
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+ ]
+});
+    
+$('.testimonials').slick({
+  autoplay: true,
+  arrows: false,
+  dots: true,
+  infinite: true,
+  speed: 1000,
+  fade: true,
+  cssEase: 'linear'
+});
+});
