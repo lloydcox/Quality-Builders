2b1d15643ec4c6e2249e87c2131c0e27346c4fc0
Dermot Wilson <dermot.wilson@netmatters-scs.com>
2020-08-17 12:28:04 +0100

    Added alert JS plugin

--- Assets/JS/contact-form.js
+++ Assets/JS/contact-form.js
@@ -1,5 +1,12 @@
+$().ready(function() {
+    //on page load uncheck any ticked checkboxes
+    $("input:checkbox:checked").attr("checked", "");
+});
+
+// Stores whether privacy policy has been accepted 
 var privacy = false;
 
+// Array to store contact details and message
 const details = [
         'name: ',
         'email: ',
@@ -10,13 +17,17 @@ const details = [
 
 // Store whether privacy policy has been agreed with
 document.querySelector(".checkbox")
-.addEventListener("change", truefalse => {
-    privacy = !privacy;
+.addEventListener("change", function truefalse() {
+    if (this.checked) { 
+    privacy = true;
+    } else {
+        privacy = false;
+    }
     console.log(privacy);
 })
 
 // Listen for click on submit button to set values of inputs into variables
-document.querySelector(".submit").addEventListener("click", storeInfo => {
+document.querySelector(".submit").addEventListener("click", function storeInfo() {
 
     if (privacy) {
         // Attach data to Array 
@@ -25,9 +36,21 @@ document.querySelector(".submit").addEventListener("click", storeInfo => {
         }
         //  Submit data (to console for now)
         console.log(details);
+        Swal.fire({
+            title: 'Success!',
+            text: 'Your message and details have been submitted.',
+            icon: 'success',
+            confirmButtonText: 'Continue'
+          })
     } else {
         // display error message
         console.log("You need to accept the privacy policy!")
+        Swal.fire({
+            title: 'Error!',
+            text: 'You need to accept the privacy policy to submit your details!',
+            icon: 'error',
+            confirmButtonText: 'Continue'
+          })
     }
  })
 
